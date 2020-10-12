50dee9ed83dabb07a4cc17ed901d2d71cdf9f1ea
Jailynn <72613082+JailynnPrimus@users.noreply.github.com>
2020-10-12 17:31:14 -0400

    Initializing Values for Loop & Beginning Loop Construction
    Edited userInput space to 11 in order to include end of line value. Initialized $s2 in order to increment in Loop in order to get each character value. $s3 will be used to close loop.

--- project1.s
+++ project1.s
@@ -1,14 +1,21 @@
 .data
-      userInput: .space  10 
+      userInput: .space  11 
 .text
      main:
           #Getting user's input as text
           li $v0, 8
           la $a0, userInput
-          li $a1, 10
+          li $a1, 11
           syscall
-     
- 
+          
+          add $s2, $zero, $zero
+          addi $s3, $zero, 11
+          Loop:
+               la $a1, userInput
+               addu $a1, $a1, $s2
+               lbu $a0,($a1)
+
+          
 
      #End of main
      li $v0, 10
