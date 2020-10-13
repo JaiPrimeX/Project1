e35974721aa595de117a0c9d15c743c733f1e59d
Jailynn <72613082+JailynnPrimus@users.noreply.github.com>
2020-10-13 11:40:50 -0400

    Sectioning Code by Ascii Numbering
    Initialized the output register and dividing the code into parts. In the Acsii chart UpperCase are 65-85 for my Mth letter U. 97-117 for the lowercase respectively. Anything not within these limits must equal zero in order to not impact the output value.

--- project1.s
+++ project1.s
@@ -11,13 +11,22 @@
           #Initializing Values Before Loop
           add $s0, $zero, $zero            #The Value that will be incremented
           addi $s1, $zero, 10              #Closing condition for loop
+          add $t0, $zero, $zero            #Initializing Output Value Register
+          
 
           Loop:
                lb $s6, userInput($s0)
-               
+          Return_here:   
                addi $s0, $s0, 1
                bne $s0, $s1, Loop
                j Exit
+          1to9Handler:
+          
+          UppercaseHandler:
+          
+          LowercaseHandler:
+
+          AnythingElseHandler:
 
        Exit:   
           #End of main
